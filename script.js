baa8fbdaea055fab79349f9e3bef1ee0a2d688b4
macca1b <brendanmaccabe@gmail.com>
2025-03-10 15:00:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
   const themeToggle = document.getElementById('theme-toggle');
-  themeToggle.addEventListener('click', function() {
+  themeToggle.addEventListener('click', () => {
     const htmlElement = document.documentElement;
     const currentTheme = htmlElement.getAttribute('data-theme');
     htmlElement.setAttribute('data-theme', currentTheme === 'light' ? 'dark' : 'light');
